f143b4e187096b4a3a4cb0f1a0ed801ca861a055
Oluwajuwon <kingzmentechnology@gmail.com>
2022-02-21 09:54:16 +0100

    modification

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"log"
 	"math/rand"
+	"os"
 
 	"github.com/floydjones1/auth-server/database"
 	"github.com/floydjones1/auth-server/routes"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
 )
  
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
@@ -23,17 +26,20 @@ func randSeq(n int) string {
 func main() {
 	
 	database.Connect()
- 
+	err:=godotenv.Load()
+	if err != nil {
+		 log.Fatal("Error loading .env files")
+	}
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
 		AllowCredentials: true,
 }))
  
 	routes.Setup(app)
-	
+	port:=os.Getenv("PORT")
 
 
-	if err := app.Listen(":4000"); err != nil {
+	if err := app.Listen(":"+port); err != nil {
 		panic(err)
 	}
 	

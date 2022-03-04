a94dc964b133a6406e2b84cc1531c39b11b32b3f
Oluwajuwon <kingzmentechnology@gmail.com>
2022-03-04 16:36:48 +0100

    implement aws image recognition

--- controller/imageController.go
+++ controller/imageController.go
@@ -2,57 +2,135 @@ package controller
 
 import (
 	"encoding/base64"
+	"encoding/json"
+	"log"
 	"os"
 	"strings"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/rekognition"
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 	"github.com/kingztech2019/nia_backend/database"
 	"github.com/kingztech2019/nia_backend/model"
 )
 
-
 type ImageRequest struct {
-	FirstImage string  
-  
-	SecondImage string  
-	ThirdImage  string  
-	FourthImage string  
+	FirstImage string
+
+	SecondImage string
+	ThirdImage  string
+	FourthImage string
 }
+
+type Analysis struct {
+	LabelModelVersion string `json:"labelmodelversion"`
+	Labels            []struct {
+		Confidence float64 `json:"confidence"`
+		Name       string  `json:"name"`
+		Parents    []struct {
+			Name string `json:"name"`
+		}
+	}
+}
+
 var fileName string
-var firstImage,secondImage,thirdImage,fourthImage string
+var filtered = []string{}
+var data []string
+var firstImage, secondImage, thirdImage, fourthImage string
+var firstAnalysis, secondAnalysis, thirdAnalysis, fourthAnalysis []byte
+
+//THIS FUNCTION CONVERT THE IMAGES TO A CORRECT JSON FORMAT
+func ImageValidator(scannedimage []byte) interface{} {
+	var dat Analysis
+	if err := json.Unmarshal(scannedimage, &dat); err != nil {
+		panic(err)
+	}
+	result := dat
+	for _, value := range result.Labels {
+		for _, val := range value.Parents {
+
+			if val.Name == "Car" && value.Confidence >= float64(90) || val.Name == "Vehicle" && value.Confidence >= float64(90) {
+				filter := append(filtered, val.Name)
+				//fmt.Println(filter)
+				return filter
+
+			}
+
+		}
+	}
+
+	return nil
+}
+
+//THIS FUNCTION USE AMAZON IMAGE RECOGNITIONTO DETECT THE IMAGE
+func imageScanner(image string) []byte {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env files")
+	}
+	sess := session.New(&aws.Config{
+		Region: aws.String("us-east-1"),
+		Credentials: credentials.NewStaticCredentials(
+			os.Getenv("AccessKeyID"),
+			os.Getenv("SecretAccessKey"),
+			""),
+	})
+
+	svc := rekognition.New(sess)
+
+	decodedImage, err := base64.StdEncoding.DecodeString(image)
+
+	if err != nil {
+
+		return []byte(err.Error())
+	}
+
+	// Send request to Rekognition.
+	input := &rekognition.DetectLabelsInput{
+		Image: &rekognition.Image{
+			Bytes: decodedImage,
+		},
+	}
+
+	result, err := svc.DetectLabels(input)
+	if err != nil {
+		return []byte(err.Error())
+	}
+	output, err := json.Marshal(result)
+	if err != nil {
+
+		return []byte(err.Error())
+	}
+	//log.Println(output)
+	return output
+
+}
 
 //This function convert base64 to image
-func imageReciever(image string) string{
-	 
+func imageReciever(image string) string {
+
 	dec, err := base64.StdEncoding.DecodeString(image)
 	dirs := []string{"", "uploads"}
 	for _, dir := range dirs {
-	   f, err := os.CreateTemp(dir, "image-*.png")
-	   if err != nil {
-		  panic(err)
-	   }
-	   defer f.Close()
-	    
+		f, err := os.CreateTemp(dir, "image-*.png")
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
 
-	   if _, err := f.Write(dec); err != nil {
-		panic(err)
-	}
-	if err := f.Sync(); err != nil {
-		panic(err)
-	}
-	fileName=f.Name()
-	
-	// log.Println("NAME",f.Name())
-	// log.Println("http://localhost:4000/public/"+f.Name())
+		if _, err := f.Write(dec); err != nil {
+			panic(err)
+		}
+		if err := f.Sync(); err != nil {
+			panic(err)
+		}
+		fileName = f.Name()
 
-	// // go to begginng of file
-	// f.Seek(0, 0)
-
-	// // output file contents
-	// io.Copy(os.Stdout, f)
 	}
-	 
-	// f, err := os.Create(fmt.Sprintf("./utils/%s-fileme.png",randSeq(4)))
+
 	if err != nil {
 		panic(err)
 	}
@@ -61,64 +139,92 @@ func imageReciever(image string) string{
 	return fileName
 }
 
+//THIS FUNCTION RETURN THE FINAL RESULT
 func UploadImage(c *fiber.Ctx) error {
-	verifyCode:= c.Query("id")
-	 
+	verifyCode := c.Query("id")
+
 	req := new(ImageRequest)
 	if err := c.BodyParser(req); err != nil {
 		return err
 	}
-	// ||req.ThirdImage==""||req.FourthImage=="" 
+	// ||req.ThirdImage==""||req.FourthImage==""
 
-	if  req.FirstImage == "" || req.SecondImage == ""{
-		return fiber.NewError(fiber.StatusBadRequest, "invalid images credentials")
+	if req.FirstImage == "" || req.SecondImage == "" {
+		return fiber.NewError(fiber.StatusBadRequest, "invalid images payload")
 	}
-	
-	
-//Get the user verification code
-var identitycode model.VerifyCode
-database.DB.Where("identity_code=?", strings.TrimSpace(verifyCode)).Find(&identitycode)
-if identitycode.UserID ==0{
-	c.Status(404)
-	 return c.JSON(fiber.Map{
-	  "message": "user doesn't exist",
-	})
-	
-	
-  }
-  if req.FirstImage !="" {
+
+	//Get the user verification code
+	var identitycode model.VerifyCode
+	database.DB.Where("identity_code=?", strings.TrimSpace(verifyCode)).Find(&identitycode)
+	if identitycode.UserID == 0 {
+		c.Status(404)
+		return c.JSON(fiber.Map{
+			"message": "user doesn't exist",
+		})
+
+	}
+	if req.FirstImage != "" {
+		firstAnalysis = imageScanner(req.FirstImage)
+		if ImageValidator(firstAnalysis) == nil {
+			c.Status(400)
+			return c.JSON(fiber.Map{
+				"message": "Unable to identify the first image uploaded please try again",
+			})
+		}
+
+	}
+	if req.SecondImage != "" {
+
+		secondAnalysis = imageScanner(req.SecondImage)
+		if ImageValidator(secondAnalysis) == nil {
+			c.Status(400)
+			return c.JSON(fiber.Map{
+				"message": "Unable to identify the second image uploaded please try again",
+			})
+		}
+
+	}
+	if req.ThirdImage != "" {
+		thirdAnalysis = imageScanner(req.ThirdImage)
+		if ImageValidator(thirdAnalysis) == nil {
+			c.Status(400)
+			return c.JSON(fiber.Map{
+				"message": "Unable to identify the third image uploaded please try again",
+			})
+		}
+
+	}
+	if req.FourthImage != "" {
+		fourthAnalysis = imageScanner(req.FourthImage)
+		if ImageValidator(fourthAnalysis) == nil {
+			c.Status(400)
+			return c.JSON(fiber.Map{
+				"message": "Unable to identify the fourth image uploaded please try again",
+			})
+		}
+
+	}
+
+	//THIS METHOD CREATE AND SAVE THE IMAGES
 	firstImage = imageReciever(req.FirstImage)
-
-	
-}
-if req.SecondImage !="" {
 	secondImage = imageReciever(req.SecondImage)
-
-	
-}
-if req.ThirdImage !="" {
 	thirdImage = imageReciever(req.ThirdImage)
-
-	
-}
-if req.FourthImage !="" {
 	fourthImage = imageReciever(req.FourthImage)
+	var uploadstatus model.UploadStatus
 
-	
-}
-var uploadstatus model.UploadStatus
- 
-database.DB.Model(&uploadstatus).Where("identity_code=?",  strings.TrimSpace(verifyCode)).Update("upload_status", "active")
+	database.DB.Model(&uploadstatus).Where("identity_code=?", strings.TrimSpace(verifyCode)).Update("upload_status", "active")
 
-  images := &model.ImagesUrl{
-	 FirstImage: firstImage,
-	 SecondImage: secondImage,
-	 ThirdImage: thirdImage,
-	 FourthImage: fourthImage,
-	 UserID: identitycode.UserID,
-}
+	images := &model.ImagesUrl{
+		FirstImage:  firstImage,
+		SecondImage: secondImage,
+		ThirdImage:  thirdImage,
+		FourthImage: fourthImage,
+		UserID:      identitycode.UserID,
+	}
 
-database.DB.Create(images)
-    
-return c.JSON(fiber.Map{"message": "Image uploaded Successfully"})
+	database.DB.Create(images)
+
+	return c.JSON(fiber.Map{
+		"message": "Image uploaded successfully",
+	})
 }
--- controller/personalController.go
+++ controller/personalController.go
@@ -14,42 +14,42 @@ import (
 	"github.com/kingztech2019/nia_backend/model"
 )
 
-type VinCode struct{
+type VinCode struct {
 	Vin string `json:vin`
 }
 type Post struct {
-	Make string `json:"make"`
-	Model     string    `json:"model"`
-	Year  string `json:"year"`
-	engine   string `json:"engine"`
-   }
+	Make   string `json:"make"`
+	Model  string `json:"model"`
+	Year   string `json:"year"`
+	engine string `json:"engine"`
+}
 
-   type Data struct {
-	 Value int `json:"value"`
-   }
+type Data struct {
+	Value int `json:"value"`
+}
 
 // func TestTrue(value string,c fiber.Ctx, model string,year string  ) error {
 // 	log.Println("THIS IS VALUE",value)
 // 	postData, _ := json.Marshal(map[string]string{
 // 		"make":value,
 // 		"year":  year ,
-// 		"model": model,
+// 		"model": model
 // 	 })
-	  
-// 	 responseData := bytes.NewBuffer(postData) 
-	
+
+// 	 responseData := bytes.NewBuffer(postData)
+
 //   dataresp, err := http.Post("https://truevalue.octamile.com/analysis2","application/json",
 //   responseData)
 //   log.Println("LLOOK",responseData)
-//    if err != nil {  
-	   
+//    if err != nil {
+
 // 	 log.Printf("Request Failed: %s", err)
 // 	 return nil
 //    }
 
 //    defer dataresp.Body.Close()
 //    dataBody, err := ioutil.ReadAll(dataresp.Body)
-//    // Log the request body 
+//    // Log the request body
 //    databodyString := string(dataBody)
 //    log.Print(databodyString)
 //    // Unmarshal result
@@ -63,116 +63,110 @@ type Post struct {
 // //    return c.JSON(fiber.Map{
 // // 	   "message":data.Value,
 // //    })
-   
 
 // return nil
-	
+
 // }
 func CheckVin(c *fiber.Ctx) error {
 	req := new(VinCode)
 	if err := c.BodyParser(req); err != nil {
 		return err
 	}
-	if  req.Vin == "" {
+	if req.Vin == "" {
 		return fiber.NewError(fiber.StatusBadRequest, "invalid signup credentials")
 	}
 	postBody, _ := json.Marshal(map[string]string{
-		"vin":req.Vin,
-		"email":  "tester",
+		"vin":      req.Vin,
+		"email":    "tester",
 		"password": "tester",
-	 })
-	 responseBody := bytes.NewBuffer(postBody)
-  resp, err := http.Post("https://autorescue.ng/dealer/v2/vinprev","application/json",
-  responseBody)
-   if err != nil {   
-	 log.Printf("Request Failed: %s", err)
-	 return nil
-   }
-	 //defer resp.Body.Close()
+	})
+	responseBody := bytes.NewBuffer(postBody)
+	resp, err := http.Post("https://autorescue.ng/dealer/v2/vinprev", "application/json",
+		responseBody)
+	if err != nil {
+		log.Printf("Request Failed: %s", err)
+		return nil
+	}
+	//defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
-	// Log the request body 
+	// Log the request body
 	bodyString := string(body)
 	log.Print(bodyString)
 	// Unmarshal resultresponseData
 	post := Post{}
 	err = json.Unmarshal(body, &post)
 	if err != nil {
-	   log.Printf("Reading body failed: %s", err)
-	   return nil
+		log.Printf("Reading body failed: %s", err)
+		return nil
 	}
 	postData, _ := json.Marshal(map[string]string{
-		"make":post.Make,
-		"year":  post.Year ,
+		"make":  post.Make,
+		"year":  post.Year,
 		"model": post.Model,
-	 })
-	  
-	 responseData := bytes.NewBuffer(postData) 
-	log.Println("LLOOK",responseData)
-  dataresp, err := http.Post("https://truevalue.octamile.com/analysis2","application/json",
-  responseData)
-  
-   if err != nil {  
-	   
-	 log.Printf("Request Failed: %s", err)
-	 return nil
-   }
+	})
 
-   //defer dataresp.Body.Close()
-   dataBody, _ := ioutil.ReadAll(dataresp.Body)
-   // Log the request body 
-   databodyString := string(dataBody)
-   log.Print(databodyString)
-   // Unmarshal result
-    data:=Data{}
-   err = json.Unmarshal(dataBody, &data)
-   if err != nil {
-	  log.Printf("Reading body failed: %s", err)
-	  return nil
-   }
- 
+	responseData := bytes.NewBuffer(postData)
+	log.Println("LLOOK", responseData)
+	dataresp, err := http.Post("https://truevalue.octamile.com/analysis2", "application/json",
+		responseData)
+
+	if err != nil {
+
+		log.Printf("Request Failed: %s", err)
+		return nil
+	}
+
+	//defer dataresp.Body.Close()
+	dataBody, _ := ioutil.ReadAll(dataresp.Body)
+	// Log the request body
+	databodyString := string(dataBody)
+	log.Print(databodyString)
+	// Unmarshal result
+	data := Data{}
+	err = json.Unmarshal(dataBody, &data)
+	if err != nil {
+		log.Printf("Reading body failed: %s", err)
+		return nil
+	}
+
 	return c.JSON(fiber.Map{
-		"data":fiber.Map{
-			"firstcheck":bodyString,
-			"finalcheck":databodyString,
+		"data": fiber.Map{
+			"firstcheck": bodyString,
+			"finalcheck": databodyString,
 		},
-			
-		
- 	})
-	//return nil 
-	 
-	
+	})
+	//return nil
+
 }
- 
 
 func PersonalDetails(c *fiber.Ctx) error {
 	var data map[string]string
-	
-	if err:=c.BodyParser(&data); err != nil {
-	  fmt.Println("Unable to parse body")
-	}
-	user:=c.Locals("user").(*jwt.Token)
-	claims:=user.Claims.(jwt.MapClaims)
-	id:=claims["user_id"].(float64)
 
-	personalData:=model.PersonalDetails{
-		UserID: id,
-		FirstName: data["first_name"],
-		LastName: data["last_name"],
-		IdCode: data["id_code"],
-		MeansOfId: data["means_of_id"],
-		MiddleName: data["middle_name"],
+	if err := c.BodyParser(&data); err != nil {
+		fmt.Println("Unable to parse body")
+	}
+	user := c.Locals("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	id := claims["user_id"].(float64)
+
+	personalData := model.PersonalDetails{
+		UserID:      id,
+		FirstName:   data["first_name"],
+		LastName:    data["last_name"],
+		IdCode:      data["id_code"],
+		MeansOfId:   data["means_of_id"],
+		MiddleName:  data["middle_name"],
 		PhoneNumber: data["phone_number"],
-		Email: data["email"],
-		State: data["state"],
-		Lga: data["lga"],
-		Address: data["address"],
-		Title: data["title"],
-
+		Email:       data["email"],
+		State:       data["state"],
+		Lga:         data["lga"],
+		Address:     data["address"],
+		Title:       data["title"],
 	}
 
-	 database.DB.Create(&personalData)
-	 return c.JSON(fiber.Map{
-		 "Message":"Details save successfully",
-	 })
-	
+	database.DB.Create(&personalData)
+	return c.JSON(fiber.Map{
+		"Message": "Details save successfully",
+	})
+
 }
